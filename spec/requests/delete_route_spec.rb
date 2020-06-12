8220f6baf680b41b254f03cf9145bde7d0b49ad1
shonburgundy <seanbeloussow@yahoo.com>
2020-06-12 14:43:20 -0700

    add delete_route_spec with passing delete an animal test/ 200 status delete test

--- spec/requests/delete_route_spec.rb
+++ spec/requests/delete_route_spec.rb
@@ -10,6 +10,10 @@ describe 'delete route', :type => :request do
   end
 
   it 'deletes an animal' do
-    expect(JSON.parse(response.body)).to eq({"message"=>""})
+    expect(JSON.parse(response.body)).to eq({"message"=>"Animal successfully deleted!"})
+  end
+
+  it 'returns status 200 ok when delete is successfull' do
+    expect(response).to have_http_status(:ok)
   end
 end
