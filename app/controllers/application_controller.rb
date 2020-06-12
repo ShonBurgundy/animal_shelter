397a7ae9014dae573858825bc933ed7618e132e2
shonburgundy <seanbeloussow@yahoo.com>
2020-06-12 11:50:00 -0700

    add get & post route exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,11 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound do |exception|
     json_response({ message: exception.message }, :not_found)
   end
+
+
+  # ----------------- POST REQUEST EXCEPTIONS -----------------
+  rescue_from ActiveRecord::RecordInvalid do |exception|
+    json_response({ message: exception.message }, :unprocessable_entity)
+  end
   
 end
