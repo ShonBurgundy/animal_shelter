07491fcaf640fa24e58d8ff6d2d7b2f7df7d0540
shonburgundy <seanbeloussow@yahoo.com>
2020-06-12 09:57:29 -0700

    add response.rb w/ response moduel

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,13 +1,34 @@
 class AnimalsController < ApplicationController
 
   def index
-    @animals = {"Dog: Lily": "Cat: Timber"}
+    @animals = Animal.all
     json_response(@animals)
   end
 
-  private
-    def json_response(object, status = :ok)
-      render json: object, status: status
+  def show
+    @animal = Animal.find(params[:id])
+    json_response(@animal)
+  end
+
+  def create
+    @animal = Animal.create(animal_params)
+    json_response(@animal)
+  end
+
+  def update
+    @animal = Animal.find(params[:id])
+    @animal.update(animal_params)
+  end
+
+  def destroy
+    @animal = Animal.find(params[:id])
+    @animal.destroy
+  end
+
+  private 
+
+    def animal_params
+      params.permit(:dog, :cat)
     end
 
 end    
