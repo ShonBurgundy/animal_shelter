e25053f05feb3a7aa2f24fb9085ca425ee34ea2b
shonburgundy <seanbeloussow@yahoo.com>
2020-06-12 14:48:20 -0700

    pass delete route error tests

--- spec/requests/delete_route_spec.rb
+++ spec/requests/delete_route_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 require 'pry'
 
+# --------------- DELETE ROUTE TESTS ---------------
+
 describe 'delete route', :type => :request do
   
   before do
@@ -15,5 +17,21 @@ describe 'delete route', :type => :request do
 
   it 'returns status 200 ok when delete is successfull' do
     expect(response).to have_http_status(:ok)
+  end
+end
+
+# --------------- DELETE ROUTE ERROR TESTS ---------------
+
+describe 'delete route errors' , :type => :request do
+  before do
+    delete "/animals/5000"
+  end
+
+  it 'throws an error message if user does not specify animal' do
+    expect(JSON.parse(response.body)).to eq({"message"=>"Couldn't find Animal with 'id'=5000"})
+  end
+
+  it 'returns status 404 not found when delete is unsuccessfull' do
+    expect(response).to have_http_status(:not_found)
   end
 end
